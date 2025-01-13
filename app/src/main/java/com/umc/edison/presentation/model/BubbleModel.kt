d7c605b8bdead65fce1db8293b6c70b6e927e017
OSSU <o.ss.u2056@gmail.com>
2025-01-13 15:52:17 +0900

    :sparkles: Feature #3: 버블 컴포저블 개발

--- app/src/main/java/com/umc/edison/presentation/model/BubbleModel.kt
+++ app/src/main/java/com/umc/edison/presentation/model/BubbleModel.kt
@@ -3,16 +3,15 @@ package com.umc.edison.presentation.model
 import com.umc.edison.domain.model.Bubble
 
 data class BubbleModel(
-    val id: Int,
     val title: String? = null,
     val content: String? = null,
     val mainImage: String? = null,
-    val images: List<String>,
-    val labels: List<LabelModel>,
-    val date: String,
+    val images: List<String> = listOf(),
+    val labels: List<LabelModel> = listOf(),
+    val date: String
 )
 
 fun Bubble.toPresentation(): BubbleModel =
-    BubbleModel(id, title, content, mainImage, images, labels.map { it.toPresentation() }, date)
+    BubbleModel(title, content, mainImage, images, labels.map { it.toPresentation() }, date)
 
 fun List<Bubble>.toPresentation(): List<BubbleModel> = map { it.toPresentation() }
