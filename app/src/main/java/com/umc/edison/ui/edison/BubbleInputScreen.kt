cdf9dc1c31d7afa3684b7066325016ee2c4330df
HwangJaemin49 <ch06159@gmail.com>
2025-09-07 14:31:46 +0900

    [Refactor] 수정

--- app/src/main/java/com/umc/edison/ui/edison/BubbleInputScreen.kt
+++ app/src/main/java/com/umc/edison/ui/edison/BubbleInputScreen.kt
@@ -323,6 +323,8 @@ fun BubbleInputContent(
         onEnterPressed = { index -> viewModel.onEnterAt(index) },
         onGapTapped = { leftIndex, rightIndex ->
             viewModel.onGapTapped(leftIndex, rightIndex)
-        }
+        },
+        onFocusRequestHandled = { viewModel.clearFocusedIndex() }
+
     )
 }
