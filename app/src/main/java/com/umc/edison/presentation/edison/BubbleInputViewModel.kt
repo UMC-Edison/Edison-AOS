e6fd7016a531646734804128d984b6fdf7bbb160
HwangJaemin49 <ch06159@gmail.com>
2025-10-12 00:09:42 +0900

    FIX(#111): 충돌 수정

--- app/src/main/java/com/umc/edison/presentation/edison/BubbleInputViewModel.kt
+++ app/src/main/java/com/umc/edison/presentation/edison/BubbleInputViewModel.kt
@@ -20,7 +20,6 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
-import java.util.LinkedList
 import javax.inject.Inject
 
 private sealed class InsertionTarget {
